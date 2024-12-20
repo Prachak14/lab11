299f1e0b7a5b8da13725b6eac8d2563a23b7942f
Your Name <you@example.com>
2024-12-20 15:50:18 +0700

    lab11

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main() {
+    
+    srand(time(0));
+
+   
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    cout << "Press Enter 3 times to reveal your future.";
+
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    string futureGrade = grades[rand() % 9];
+
+    
+    cout << "\nYou will get " << futureGrade << " in this 261102.\n";
+
+    return 0;
+}
